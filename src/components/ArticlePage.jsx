cf33397626623f4ed1df6ce7b1a4d4f42638ebd6
smlbrd <smallbirdquickthorn+github@gmail.com>
2024-12-16 22:03:05 +0000

    link header logo to home

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -32,14 +32,18 @@ function ArticlePage() {
 
   return (
     <>
-      <h1>{article.title}</h1>
-      <img src={article_img_url} alt={`Banner image for ${title}`} />
-      <h2>{author}</h2>
-      <div>{date}</div>
-      <div>{body}</div>
-      <div>{topic}</div>
-      <div>{votes} votes</div>
-      <div>{comment_count} comments</div>
+      <h1 className="article-title">{article.title}</h1>
+      <img
+        className="article-image"
+        src={article_img_url}
+        alt={`Banner image for ${title}`}
+      />
+      <h2 className="article-author">{author}</h2>
+      <div className="article-date">{date}</div>
+      <div className="article-body">{body}</div>
+      <div className="article-topic">{topic}</div>
+      <div className="article-votes">{votes} votes</div>
+      <div className="article-comment-count">{comment_count} comments</div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router';
+
 function Header() {
   return (
     <div className="header">
-      <h1>NC News</h1>
+      <Link to={`/articles`}>
+        <h1>NC News</h1>
+      </Link>
     </div>
   );
 }
