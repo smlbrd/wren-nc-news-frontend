9db99ac003923301ba7b5ac56e831c9c30a6b309
smlbrd <smallbirdquickthorn+github@gmail.com>
2024-12-17 09:41:27 +0000

    loading states added

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,12 +4,15 @@ import ArticleCard from '../components/ArticleCard';
 import { getAllArticles } from '../api/api';
 
 function ArticleList() {
+  const [isLoading, setIsLoading] = useState(false);
   const [articleList, setArticleList] = useState([]);
 
   useEffect(() => {
+    setIsLoading(true);
     getAllArticles()
       .then(({ articles }) => {
         setArticleList(articles);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(error, 'ruh roh!');
@@ -17,13 +20,25 @@ function ArticleList() {
   }, []);
 
   return (
-    <ul className="card-container">
-      {articleList.map((article, index) => {
-        return (
-          <ArticleCard key={article.article_id} cardId={index} {...article} />
-        );
-      })}
-    </ul>
+    <>
+      <>
+        {isLoading ? (
+          <p className="loading">Starting to spread the news...</p>
+        ) : (
+          <ul className="card-container">
+            {articleList.map((article, index) => {
+              return (
+                <ArticleCard
+                  key={article.article_id}
+                  cardId={index}
+                  {...article}
+                />
+              );
+            })}
+          </ul>
+        )}
+      </>
+    </>
   );
 }
 
--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -5,13 +5,16 @@ import { useParams } from 'react-router';
 import { getArticleById } from '../api/api';
 
 function ArticlePage() {
+  const [isLoading, setIsLoading] = useState(false);
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
 
   useEffect(() => {
+    setIsLoading(true);
     getArticleById(article_id)
       .then(({ article }) => {
         setArticle(article);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(error, 'ruh roh!');
@@ -33,18 +36,31 @@ function ArticlePage() {
 
   return (
     <>
-      <h1 className="article-title">{article.title}</h1>
-      <img
-        className="article-image"
-        src={article_img_url}
-        alt={`Banner image for ${title}`}
-      />
-      <h2 className="article-author">{author}</h2>
-      <div className="article-date">{date}</div>
-      <div className="article-body">{body}</div>
-      <div className="article-topic">{topic}</div>
-      <div className="article-votes">{votes} votes</div>
-      <div className="article-comment-count">{comment_count} comments</div>
+      <>
+        {isLoading ? (
+          <p className="loading">
+            Watching thousands of pages zoom past like the newspaper machines in
+            the movies...
+          </p>
+        ) : (
+          <>
+            <h1 className="article-title">{article.title}</h1>
+            <img
+              className="article-image"
+              src={article_img_url}
+              alt={`Banner image for ${title}`}
+            />
+            <h2 className="article-author">{author}</h2>
+            <div className="article-date">{date}</div>
+            <div className="article-body">{body}</div>
+            <div className="article-topic">{topic}</div>
+            <div className="article-votes">{votes} votes</div>
+            <div className="article-comment-count">
+              {comment_count} comments
+            </div>
+          </>
+        )}
+      </>
     </>
   );
 }
